053fbbfd27695106318eb2cd75e561ac8ef0fdba
Aravind <aravindsprint@gmail.com>
2022-10-03 11:48:38 +0530

    updated patches

--- gym_management/patches/post_install/v1_0/add_cost_center_in_gym_membership.py
+++ gym_management/patches/post_install/v1_0/add_cost_center_in_gym_membership.py
@@ -5,7 +5,7 @@ def execute():
 	frappe.reload_doc("gym_management", "doctype", "gym_membership")
 	gym_membership = frappe.qb.DocType("Gym Membership")
 
-	posted_date = "30-09-2022"
+	posted_date = "2022-09-30"
 	default_cost_center = "Main - TMF"
 	frappe.qb.update(gym_membership).set(gym_membership.cost_center, default_cost_center).where(
 		gym_membership.posting_date <= posted_date
